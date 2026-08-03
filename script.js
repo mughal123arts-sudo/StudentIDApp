5ec4a7292a7fc7136f8bd88d219f50f2fcf102df
mughal123arts-sudo <mughal123arts@gmail.com>
2026-08-03 02:27:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const images = [
+"WhatsApp Image 2026-08-03 at 12.40.10 AM (1).jpeg",
+"WhatsApp Image 2026-08-03 at 12.40.10 AM (2).jpeg",
+"WhatsApp Image 2026-08-03 at 12.40.10 AM.jpeg"
+];
 
+let current = 0;
+
+const slide = document.getElementById("slide");
+
+function changeSlide() {
+
+current++;
+
+if (current >= images.length) {
+current = 0;
+}
+
+slide.src = images[current];
+
+}
+
+setInterval(changeSlide,3000);
